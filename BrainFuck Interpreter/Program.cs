75f45e7e76c986c5168f523ad9c0e342abdd964c
Kiss Tamás <tamaskiss13@gmail.com>
2016-09-02 09:30:08 +0200

    Optimalize >< and +- symbols

--- BrainFuck Interpreter/Program.cs
+++ BrainFuck Interpreter/Program.cs
@@ -15,70 +15,153 @@ namespace BrainFuck_Interpreter
             int ip = 0; // instruction pointer
             int dp = 0; //data pointer
 
-            while(input[ip] != '0')  // '0' char is the eof signal
-            {
-                int nesting = 0;
-                char c = input[ip]; //current operation
+            input = optimalize(input);
 
-                switch (c)
+            try
+            {
+                while (input[ip] != '0') // '0' char is the eof signal
                 {
-                    case '>':
-                        dp++;
-                        break;
-                    case '<':
-                        dp--;
-                        break;
-                    case '+':
-                        memory[dp]++;
-                        break;
-                    case '-':
-                        memory[dp]--;
-                        break;
-                    case '.':
-                        Console.Write(Convert.ToChar(memory[dp]));
-                        break;
-                    case ',':
-                        memory[dp] = Console.Read();
-                        break;
-                    case '[':
-                        if (memory[dp] == 0)
-                            for (int j = ip+1; j < input.Length; j++)  //look forward and find corresponding ']'
-                            {
-                                if (input[j] == '[') //We've found a nested for
-                                    nesting++;
-                                else if (input[j] == ']') //Resolve one layer, further checking if this is the nest we are looking for
+                    int nesting = 0;
+                    char c = input[ip]; //current operation
+
+                    switch (c)
+                    {
+                        case '>':
+                            dp++;
+                            break;
+                        case '<':
+                            dp--;
+                            break;
+                        case '+':
+                            memory[dp]++;
+                            break;
+                        case '-':
+                            memory[dp]--;
+                            break;
+                        case '.':
+                            Console.Write(Convert.ToChar(memory[dp]));
+                            break;
+                        case ',':
+                            memory[dp] = Console.Read();
+                            break;
+                        case '[':
+                            if (memory[dp] == 0)
+                                for (int j = ip + 1; j < input.Length; j++) //look forward and find corresponding ']'
                                 {
-                                    if (nesting == 0)
+                                    if (input[j] == '[') //We've found a nested for
+                                        nesting++;
+                                    else if (input[j] == ']')
+                                    //Resolve one layer, further checking if this is the nest we are looking for
                                     {
-                                        ip = j;  //If it is, jump there
-                                        break;
+                                        if (nesting == 0)
+                                        {
+                                            ip = j; //If it is, jump there
+                                            break;
+                                        }
+                                        nesting--; //If not, reduce nesting
                                     }
-                                    nesting--; //If not, reduce nesting
                                 }
-                            }
-                        break;
-                    case ']':
-                        if (memory[dp] != 0)
-                            for (int j = ip - 1; j >= 0; j--)
-                            {
-                                if (input[j] == ']')
-                                    nesting++;
-                                else if (input[j] == '[')
+                            break;
+                        case ']':
+                            if (memory[dp] != 0)
+                                for (int j = ip - 1; j >= 0; j--)
                                 {
-                                    if (nesting == 0)
+                                    if (input[j] == ']')
+                                        nesting++;
+                                    else if (input[j] == '[')
                                     {
-                                        ip = j-1; // -1 beacuse ip is increased at the end of the loop
-                                        break;
+                                        if (nesting == 0)
+                                        {
+                                            ip = j - 1; // -1 beacuse ip is increased at the end of the loop
+                                            break;
+                                        }
+                                        nesting--;
                                     }
-                                    nesting--;
                                 }
-                            }
-                        break;
+                            break;
+                    }
+                    ip++;
                 }
-                ip++;
             }
+            catch (IndexOutOfRangeException)
+            {
+                if (ip >= input.Length)
+                    Console.WriteLine("EOF not found.");
+                else
+                {
+                    Console.WriteLine("Unspecified IndexOutOfRange Exception");
+                    throw;
+                }
+            }
+
 
             Console.ReadKey();
         }
+
+        private static string optimalize(string input)
+        {
+            string optimalized = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int lastInSequence;
+
+                switch (input[i])
+                {
+                    case '>':
+                    case '<':
+                        optimalized += Simplify(input, '>', '<', i, out lastInSequence);
+                        i += lastInSequence - i - 1;
+                        break;
+                    case '+':
+                    case '-':
+                        optimalized += Simplify(input, '+', '-', i, out lastInSequence);
+                        i += lastInSequence - i - 1;
+                        break;
+                    case '.':
+                    case ',':
+                    case '[':
+                    case ']':
+                    case '0':
+                        optimalized += input[i];
+                        break;
+                }
+            }
+
+            return optimalized;
+        }
+
+        private static string Simplify(string input, char left, char right, int where, out int j)
+        {
+            int leftNumber = 0;
+
+            for (j = where; j < input.Length && (input[j] == left || input[j] == right); j++)
+            {
+                if (input[j] == left)
+                    leftNumber++;
+                else
+                    leftNumber--;
+            }
+
+            if (j == where + 1)
+                return input[where].ToString();
+
+            return AddItem(left, right, leftNumber);
+        }
+
+        private static string AddItem(char charLeft, char charRight, int leftNumber)
+        {
+            string result = "";
+
+            if (leftNumber > 0)
+                for (int i = 0; i < leftNumber; i++)
+                    result += charLeft;
+            else
+                for (int i = 0; i < -1*leftNumber; i++)
+                    result += charRight;
+
+            return result;
+        }
     }
 }
+
